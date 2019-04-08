f54cabed3bd6d63c10892ec693e3d2b0b1501b3f
virus <___>
2019-04-08 11:37:43 +0200

    updated page title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,7 +18,7 @@ class Trality extends App {
                         <Container>
                             <GlobalStyle />
                             <Head>
-                                <title>trality. - build & follow bots</title>
+                                <title>Trality. - build & follow bots</title>
                                 <meta charSet="UTF-8" />
                                 <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, user-scalable=no" />
                                 <meta
