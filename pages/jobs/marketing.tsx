6dd126583395d31d6e24bea43b042f6d24a87be5
virus <___>
2019-10-07 19:01:31 +0200

    Fixed jobs

--- pages/jobs/marketing.tsx
+++ pages/jobs/marketing.tsx
@@ -18,6 +18,48 @@ export default () => {
                         ]}
                     />
                     <Header title="Manager Marketing & Communication (Part-time)">
+                        <p>
+                            We’re looking for talented marketing or PR students, who work on marketing (Social, SEO and Brand) as well as
+                            communication and PR directly together with the CEO of Trality. If you would love to help us spread the word on our
+                            startup and be responsible for driving awareness and user growth through creativity and data-driven marketing methods, you
+                            are the perfect match for this position!
+                        </p>
+                        <h3>What you will do everyday</h3>
+                        <>
+                            <ul>
+                                <li>Work on growth marketing and communications</li>
+
+                                <li>Take control of Trality’s marketing channels and develop ideas to grow awareness and user base</li>
+                                <li>Write Press Releases as well as marketing content, based on news, statistics and more</li>
+                                <li>Liaise directly with journalists in Austria as well as globally</li>
+                            </ul>
+                        </>
+                        <h3>What you bring to the table</h3>
+                        <>
+                            <ul>
+                                <li>You have a strong passion for PR, marketing and content writing</li>
+                                <li>You are a Business, Communications, IT, Media or PR student (or similar)</li>
+                                <li>
+                                    You have a very good command of English (spoken and written language) and are at least able to converse with your
+                                    team in German
+                                </li>
+                                <li>You are interested in start-ups and entrepreneurship</li>
+                                <li>You work independently and like to take the initiative</li>
+                                <li>You don’t mind having a beer with us on Friday afternoons after work is done</li>
+                            </ul>
+                        </>
+                        <h3>What you'll get/our offer</h3>
+                        <>
+                            <ul>
+                                <li>
+                                    10h part-time job with a possibility to work remotely or at a modern desk in the most livable city in the word –
+                                    Vienna
+                                </li>
+                                <li>An extremely flexible working environment with a lot of creative freedom in further developing of our startup</li>
+                                <li>A salary of 446,81€ (per month, paid 14 times), which equals Austrian “Geringfügigkeit”</li>
+                                <li>A flexible employment length, but minimum 4 months</li>
+                            </ul>
+                        </>
                         <h3>Sounds interesting?</h3>
                         <p>
                             To apply, drop us an email including a CV and a 1-page cover letter to <b>jobs [at] trality [dot] com</b>.
--- pages/jobs/mobile.tsx
+++ pages/jobs/mobile.tsx
@@ -18,6 +18,58 @@ export default () => {
                         ]}
                     />
                     <Header title="Senior/Junior Mobile Developer (Part-/Full-time)">
+                        <p>
+                            We’re looking for a talented mobile engineer, who is willing to join the Trality team on a short-term contract (3-4 months
+                            with a possibility of extension) in order to develop the platform’s mobile app.
+                        </p>
+                        <h3>What you will do everyday</h3>
+                        <>
+                            <ul>
+                                <li>Build a mobile app for the Trality platform from scratch using React Native</li>
+
+                                <li>
+                                    Collaborate with product managers, designers, and software engineers to ensure our design vision is maintained,
+                                    final markup is standards compliant, and UI deliverables are completed on time
+                                </li>
+                                <li>Work with the real time feedback from our users (external and internal) to make the products better</li>
+                                <li>Enthuse the Trality team with your ideas and suggestions for solutions, libraries and technologies</li>
+                                <li>
+                                    Take responsibility not just for your own code, but for the entire product, always refine what’s under the hood of
+                                    our platform
+                                </li>
+                            </ul>
+                        </>
+                        <h3>What you bring to the table</h3>
+                        <>
+                            <ul>
+                                <li>You have at least 3 years of experience in mobile development</li>
+                                <li>
+                                    You have worked with React Native (and/or ReactJS) and you know your way around Git, moreover, experience with
+                                    e.g. GraphQL is a plus
+                                </li>
+                                <li>In-depth knowledge of modular, reusable and intelligent API design is also nice to have</li>
+                                <li>You work independently and like to take the initiative</li>
+                                <li>
+                                    You have experience with cross-functional teams and managing relationships with business or operations
+                                    stakeholders
+                                </li>
+                                <li>You don’t mind having a beer with us on Friday afternoons after work is done</li>
+                            </ul>
+                        </>
+                        <h3>What you'll get/our offer</h3>
+                        <>
+                            <ul>
+                                <li>
+                                    38,5h full-time or 20h part-time job with a possibility to work remotely or at a modern desk in the most livable
+                                    city in the word – Vienna
+                                </li>
+                                <li>A short-term contract length of 3-4 months, with the possibility of extension at the end of the employment</li>
+                                <li>
+                                    An extremely flexible working environment with a lot of creative freedom in the further development of our startup
+                                </li>
+                                <li>A competitive remuneration based on your qualification, education and experience</li>
+                            </ul>
+                        </>
                         <h3>Sounds interesting?</h3>
                         <p>
                             To apply, drop us an email including a CV and a 1-page cover letter to <b>jobs [at] trality [dot] com</b>.
