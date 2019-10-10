7fd766e940c23b5b3be1058dceee524989f17357
virus <___>
2019-10-10 11:32:15 +0200

    Updated faqs

--- pages/resources/faq.tsx
+++ pages/resources/faq.tsx
@@ -7,23 +7,45 @@ export default () => {
             <Layout.Section id="faq">
                 <Layout.Center>
                     <Header title="FAQ">
-                        <Layout.Collapsible title="What are trading bots anyways?">
+                        <Layout.Collapsible title="What are trading bots?">
                             Trading bots are a set of algorithms that control automated buying and selling of assets. Doing so they exploit price
                             volatility, which is particularly high in crypto assets.
                         </Layout.Collapsible>
 
                         <Layout.Collapsible title="Where do I need to deposit my crypto assets to start trading?">
                             All you need to do is deposit funds on your favorite crypto exchange and connect Trality via its official API. For more
-                            info on the first steps, please visit our documentation.{' '}
+                            info on the first steps, please visit our{' '}
+                            <a href="https://docs.trality.com/general/getting-started" target="_blank">
+                                documentation
+                            </a>
+                            .
                         </Layout.Collapsible>
 
                         <Layout.Collapsible title="Which exchanges do you support?">
                             We currently support Binance and more exchanges will follow.
                         </Layout.Collapsible>
 
-                        <Layout.Collapsible title="How do you compare yourselves to competitors such as Crypto-hopper or 3-Commas?">
-                            These and other solutions offer overly-simplified rule-based bot solutions, which provide the user only with a fraction of
-                            the possibilities that algorithmic trading offers.
+                        <Layout.Collapsible title="How do I start creating a trading bot?">
+                            At Trality, you currently have two options to create trading bots:
+                            <ul>
+                                <li>Your create bots using code using our Trality Code Editor</li>
+                                <li>Your build bots using rules using our Trality Rule Builder</li>
+                            </ul>
+                            The best starting point to dig into our tools is always our{' '}
+                            <a href="https://docs.trality.com" target="_blank">
+                                documentation
+                            </a>
+                            . Whether you use code or not, you can start creating or building your bots directly in the browser with no additional
+                            software you need to download.
+                        </Layout.Collapsible>
+
+                        <Layout.Collapsible title="What coding language do you support?">
+                            We currently support Python and are working to support even more languages in the future.
+                        </Layout.Collapsible>
+
+                        <Layout.Collapsible title="Where do trading bots run?">
+                            You have no need to setup your own servers for using your trading bots. All your bots can be run safely and reliably on
+                            Trality infrastructure, and can be monitored by you day and night.
                         </Layout.Collapsible>
 
                         <Layout.Collapsible title="How do you guarantee that the bots on the marketplace aren’t used for scamming people?">
