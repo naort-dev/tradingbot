1035c707dbd7128e8fb90997486dface4730b97f
naort-dev <naor.tzairi.upwork@gmail.com>
2020-10-20 22:53:15 +0100

    Changes to image & bundle
    -- Starting using withOptimizedImages
    -- Added analyzer option - quickly analyze a bundle
    -- add assets to sideEffects so it can be better tree shaked
    -- Adding imagemin-pngquant for PNG compression - no loss quality

--- next.config.js
+++ next.config.js
@@ -4,59 +4,65 @@ const withOptimizedImages = require('next-optimized-images');
 const TerserPlugin = require('terser-webpack-plugin');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 const withCSS = require('@zeit/next-css');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true'
+})
 
-module.exports = withCSS(
-    withImages({
-        target: 'serverless',
-        webpack(config, options) {
-            if (config.resolve.plugins) {
-                config.resolve.plugins.push(new TsconfigPathsPlugin());
-            } else {
-                config.resolve.plugins = [new TsconfigPathsPlugin()];
-            }
 
-            const { isServer } = options;
-            const prefix = config.assetPrefix ? config.assetPrefix : '';
+module.exports = withBundleAnalyzer(
+    withCSS(
+        withOptimizedImages({
+            inlineImageLimit: -1,
+            target: 'serverless',
+            webpack(config, options) {
+                if (config.resolve.plugins) {
+                    config.resolve.plugins.push(new TsconfigPathsPlugin());
+                } else {
+                    config.resolve.plugins = [new TsconfigPathsPlugin()];
+                }
 
-            config.module.rules.push(
-                {
-                    test: /\.ico$/,
-                    use: [
-                        {
-                            loader: 'file-loader',
-                            options: {
-                                context: '',
-                                emitFile: true,
-                                name: '[name].[ext]',
+                const { isServer } = options;
+                const prefix = config.assetPrefix ? config.assetPrefix : '';
+                config.module.rules.push(
+                    {
+                        test: /\.ico$/,
+                        use: [
+                            {
+                                loader: 'file-loader',
+                                options: {
+                                    context: '',
+                                    emitFile: true,
+                                    name: '[name].[ext]',
+                                },
                             },
-                        },
-                    ],
-                },
-                {
-                    test: /\.mp4$/,
-                    use: [
-                        {
-                            loader: 'file-loader',
-                            options: {
-                                context: '',
-                                emitFile: true,
-                                name: '[hash].[ext]',
-                                publicPath: `${prefix}/_next/static/videos/`,
-                                outputPath: `${isServer ? '../' : ''}static/videos/`,
+                        ],
+                    },
+                    {
+                        test: /\.mp4$/,
+                        use: [
+                            {
+                                loader: 'file-loader',
+                                options: {
+                                    context: '',
+                                    emitFile: true,
+                                    name: '[hash].[ext]',
+                                    publicPath: `${prefix}/_next/static/videos/`,
+                                    outputPath: `${isServer ? '../' : ''}static/videos/`,
+                                },
                             },
-                        },
-                    ],
-                },
-            );
-            /*config.plugins.push(
-                new webpack.EnvironmentPlugin({
-                    NODE_ENV: 'production',
-                    DEBUG: false,
-                }),
-            );*/
-            // TODO
-            // config.optimization.minimize = false;
-            return config;
-        },
-    }),
+                        ],
+                    },
+                );
+                /*config.plugins.push(
+                    new webpack.EnvironmentPlugin({
+                        NODE_ENV: 'production',
+                        DEBUG: false,
+                    }),
+                );*/
+                // TODO
+                // config.optimization.minimize = false;
+                return config;
+            },
+        }),
+    )
 );
