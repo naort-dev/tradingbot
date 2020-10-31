c21e547f56bc697bbe1185951a34262419a0b3c3
naort-dev <naor.tzairi.upwork@gmail.com>
2020-10-31 18:05:45 +0000

    Some updates based on the feedback.

--- pages/competition.tsx
+++ pages/competition.tsx
@@ -20,7 +20,7 @@ const Competition = () => {
             <Layout.Center mobileFullWidth={true}>
                 <HeroSection>
                     <Headline>Trality Trading <strong>Competition</strong></Headline>
-                    <HeroText><CalendarIcon src={Calendar} />Nov 1, 2020 - Nov 30, 2020</HeroText>
+                    <HeroText><CalendarIcon src={Calendar} />Nov 4, 2020 - Jan 14, 2021</HeroText>
                     <HeroButton to={ConstUrl.Signup}>Register Now</HeroButton>
                 </HeroSection>
             </Layout.Center>
@@ -33,9 +33,8 @@ const Competition = () => {
         <BoxesSection id="overview">
             <Layout.Center>
                 <ExtendedHeader center title="Overview">
-                    <p>Welcome to the Trality Algo <strong>Trading Challenge 2020/21.</strong></p>
-                    <p>Create a next-gen trading bot using our state-of-the-art tools and submit it to Trality before the deadline.</p>
-                    <p>Trality will provide standard sets of price data (source: <a href="javascript:alert('Where should this link go to? :)')">Binance</a>) to help with your bot development, which can be done in either Python or our easy to use drag-and-drop <a href="javascript:alert('Where should this link go to? :)')">Rule Builder</a> interface.</p>
+                    <p><strong>Create a winning trading bot using our state-of-the-art tools and get your hands on a big cash prize.</strong></p>
+                    <p>You have until the end of November to create and submit a bot after which it will run in a simulated environment for 45 days. At the end of the simulation period, the top three ranked participants according to RoMaD (Return Over Maximum Drawdown) will receive a cash prize. This cash price is related to the performance of the winning bot which we will outline below. Trality will provide standard sets of price data (source: Binance) to help with your bot development, which can be done with our <a href="/creator/code-editor">Python Code Editor</a> or our intuitive drag-and-drop <a href="/creator/rule-builder">Rule Builder</a>.</p>
                     <Layout.Grid.Container>
                         <VideoRow>
                             <TwoColGridItem>
@@ -50,14 +49,63 @@ const Competition = () => {
                             </TwoColGridItem>
                         </VideoRow>
                     </Layout.Grid.Container>
-                    <p>Our main goal is to expand the community of enthusiastic developers and tech-savvy people around crypto trading, therefore we welcome any type of traders to take part in this competition.</p>
-                    <p>Your trading system will remain confidential and remains your intellectual property (see terms and conditions*).</p>
-                </ExtendedHeader>
+                    </ExtendedHeader>
             </Layout.Center>
         </BoxesSection>
+        <Layout.Section id="how-to-participate">
+            <Layout.Center>
+                <ExtendedHeader center title="How to participate">
+                    <p>
+                        We make it very easy for users both new and existing to take part in this exciting competition. You simply have to:
+                    </p>
+                    <NonDiscList>
+                        <li>
+                            <strong>Step 1. <a target="_blank" href={ConstUrl.Signup}>Register</a></strong> for an account if you haven’t already (it takes just four clicks)
+                        </li>
+                        <li>
+                            <strong>Step 2. <a target="_blank" href={ConstUrl.Signup}>Create a bot</a></strong> using our powerful bot-creation tools
+                        </li>
+                        <li>
+                            <strong>Step 3. <a target="_blank" href={ConstUrl.Signup}>Submit the bot</a></strong> in the submission section
+                        </li>
+                    </NonDiscList>
+                </ExtendedHeader>
+            </Layout.Center>
+        </Layout.Section>
+        <Layout.Section id="creativity-pays">
+            <Layout.Center>
+                <ExtendedHeader center title="Creativity pays">
+                    <p>
+                        We recently launched our <a href="https://blog.trality.com/introducing-the-trading-engine-2-0/">Trading Engine 2.0</a> where flexibility took centre stage. In that, we provided our creators with the tools they need to build the most advanced bots possible. Trality is for those who want the most freedom when developing bots with cutting-edge technology. We attract the smartest bot creators by constantly working on innovative functionality and we stay ahead of the curve by improving our existing products and constantly introducing new services for our users.
+                    </p>
+                    <p>Our goal with this competition is to expand our community of enthusiastic developers and tech-savvy people in the industry therefore we welcome all kinds of traders to take part.</p>
+                    <p>It’s time to get creative and win!</p>
+                </ExtendedHeader>
+            </Layout.Center>
+        </Layout.Section>
         <Layout.Section id="prizes">
             <Layout.Center>
-                <ExtendedHeader center title="Prizes" subtitle="The 3 best trading bots get the following prizes:">
+                <ExtendedHeader center title="Winning and prizes">
+                    <p>
+                        At the end of the Competition Period, the top three participants according to RoMaD (Return Over Maximum Drawdown) will receive a cash prize.
+                    </p>
+                    <p>
+                        The winning metrics are calculated as follows:
+                    </p>
+                    <Formula>
+                        <p>
+                            TotalReturn / (1 + MaxDrawDown)
+                        </p>
+                        <p>
+                            TotalReturn = X_t / X_0 -1, where X_i is the capital at time i
+                        </p>
+                        <p>
+                            <strong>MaxDrawDown will be measured in absolute percent. </strong>
+                        </p>
+                    </Formula>
+                    <CenteredParagraph>
+                        <strong>There are THREE cash prizes up for 1st, 2nd and 3rd place respectively</strong>
+                    </CenteredParagraph>
                     <PrizesCenterContainer>
                         <FirstPrizePiece>
                             <PrizeText color="secondary">
@@ -87,6 +135,32 @@ const Competition = () => {
                             </PrizeMoney>
                         </PrizePiece>
                     </PrizesCenterContainer>
+                    <p>
+                        This cash prizes are related to the performance of their bot and will be calculated thusly:
+                    </p>
+                    <Table>
+                        <tr>
+                            <td><strong>1st place</strong></td>
+                            <td>EUR 20000 * min(max(performance,5%),15%)</td>
+                        </tr>
+                        <tr>
+                            <td><strong>2nd place</strong></td>
+                            <td>EUR 20000 * min(max(performance,2.5%),10%)</td>
+                        </tr>
+                        <tr>
+                            <td><strong>3rd place</strong></td>
+                            <td>EUR 20000 * min(max(performance,1.5%),5%)</td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td>
+                                <i>performance</i> = <table style={{display:"inline-table", fontSize:"65%", borderCollapse:"collapse", verticalAlign:"middle"}}><tbody><tr><td style={{padding:"0px", borderBottom: "solid 1px", width:"8px", textAlign:"center"}}>X<sub>1</sub></td></tr> <tr><td style={{padding:"0px", textAlign:"center"}}>X<sub>0</sub></td></tr></tbody></table> - 1  
+                            </td>
+                        </tr>
+                    </Table>
+                    <p>
+                        Following this prize model, the best-ranked bot creator will receive a payout of between €1000 and €3000. Second place will get €500 to €2000 and third place will get €300 to €1000.
+                    </p>
                 </ExtendedHeader>
             </Layout.Center>
         </Layout.Section>
@@ -94,63 +168,35 @@ const Competition = () => {
             <Layout.Center>
                 <ExtendedHeader center title="Competition Timeline">
                     <p>
-                        There are two main phases in the competition: <strong>The submission period</strong> and <strong>the live simulation period</strong>. All submitted trading bots will be simulated for 2 months with live market data.
-                    </p>
-                    <p>
-                        We are going to select winners who have the highest Equity value at the end of the live simulation period.
+                        There are two main phases in the competition: the <strong>submission</strong> period and the <strong>live simulation</strong> period. The submission period is the time where you can create and submit your bot to and it will go from November 4th until November 30th. The live simulation period will run from December 1st until January 14th and during this period, the bots run continuously on a virtual account. The bots submitted to this competition will NOT be trading with real assets but instead, run in a simulated environment mirroring the real market. Winners will be selected based on RoMAd (Return Over Maximum Drawdown) for the whole simulation period (1.12.2020 - 14.1.2021).
                     </p>
                     <Timeline />
                 </ExtendedHeader>
             </Layout.Center>
         </Layout.Section>
-        <Layout.Section id="format">
+        
+        <Layout.Section id="rules">
             <Layout.Center>
-                <ExtendedHeader center title="Format">
-                <Layout.Grid.Container>
-                        <TwoColGridItem>
-                            <p><strong>Winning Metric</strong> PnL(All-Time)</p>
-                            <p><strong>Initial Portfolio</strong> 100,000 USDT</p>
-                            <p><strong>Team Size</strong> 3 people, 1 submission per team</p>
-                        </TwoColGridItem>
-                        <TwoColGridItem>
-                            <p><strong>Trading Fees</strong> 0.1%</p>
-                            <p><strong>Slippage</strong> None</p>
-                            <p><strong>Packages</strong> Numpy only</p>
-                        </TwoColGridItem>
-                    </Layout.Grid.Container>
-                </ExtendedHeader>
-            </Layout.Center>
-        </Layout.Section>
-        <Layout.Section id="suggestions">
-            <Layout.Center>
-                <ExtendedHeader center title="Suggestions">
-                    <p>We encourage you to use our feature request board to make suggestions on what to build next (or report bugs :-)). </p>
-                    <p>We would love to hear what you think!</p>
-                    <KnowMore to="javascript:alert('What should this link be? :)');">Feature Request Board</KnowMore>
-                    <Layout.Grid.Container>
-                        <TwoColGridItem>
-                            <p><TickImage src={TickYes} />All intervals</p>
-                            <p><TickImage src={TickYes} />Recommended smaller than 1hr</p>
-                            <p><TickImage src={TickYes} />All bot types</p>
-                        </TwoColGridItem>
-                        <TwoColGridItem>
-                            <p><TickImage src={TickNo} />Trading illegal assets</p>
-                            <p><TickImage src={TickNo} />More than 3 handlers</p>
-                        </TwoColGridItem>
-                    </Layout.Grid.Container>
+                <ExtendedHeader center title="Eligibility">
+                    <List>
+                        <li>To be eligible to take part in the competition, participants must have a registered Trality account. If you don’t have an account yet, you can sign up here. Trality accounts are for free at the moment and you will never be charged for our services without additional consent!</li>
+                        <li>Participants must be at least 18 years old at the time of entry. Age may be verified before prize distribution.</li>
+                        <li>Participants may be from any country or region where their entry is not prohibited or restricted by law.</li>
+                        <li>There is no fee for entry and no purchase is necessary.</li>
+                    </List>
                 </ExtendedHeader>
             </Layout.Center>
         </Layout.Section>
         <Layout.Section id="rules">
             <Layout.Center>
                 <ExtendedHeader center title="Rules">
-                    <ul>
+                    <List>
                         <li>You must use the Traity Rule Editor or the Code Editor to create your bot</li>
                         <li>The bot submission deadline is <strong>November 30th at midnight CET</strong>.</li>
                         <li>Only <strong>one</strong> bot per entry</li>
                         <li>Your bot is allowed to trade in one or more of the following coins: <strong>BTC, ETH, XRP, LTC</strong> and <strong>EOS</strong>. The respective trading intervals for your bot can be chosen freely</li>
                         <li>Every bot will receive 20,000 $USDT as a starting balance</li>
-                    </ul>
+                    </List>
                     <p>
                         Read the full rules and T&amp;Cs <a href="#">here</a>.
                     </p>
@@ -188,6 +234,9 @@ const ExtendedHeader = styled(Header)`
         }
         @media only screen and (min-width: 750px) {
             margin-bottom: 25px;
+            max-width: 800px;
+            margin-left:auto;
+            margin-right: auto;
             &:last-child {
                 margin-bottom: 0px;
             }
@@ -208,8 +257,36 @@ const FirstSection = styled(Layout.Section)`
 const PrizesCenterContainer = styled(Layout.Center)`
     display:flex;
     justify-content: center;
-    max-width:850px;
+    max-width:50px;
     margin:0 auto;
+    margin-bottom: 22px;
+    @media only screen and (min-width: 750px) {
+        margin-bottom: 25px;
+    }
+`;
+
+const CenteredParagraph = styled.p`
+    text-align:center;
+`;
+
+const List = styled.ul`
+    & > li {
+        display: flex;
+        & * {
+            display: contents;
+        }
+    }
+    @media only screen and (min-width: 750px) {
+        max-width: 800px;
+        margin-left: auto;
+        margin-right: auto;
+    }
+`;
+
+const NonDiscList = styled(List)`
+    & > li:before {
+        content: ' ';
+    }
 `;
 
 const PrizePiece = styled.div`
@@ -220,6 +297,15 @@ const PrizePiece = styled.div`
         margin-left: 100px;
     }
     text-align:center;
+`;
+
+const Table = styled.table`
+    margin-top: 25px;
+    margin-bottom: 25px;
+    @media only screen and (min-width: 768px) {
+        max-width: 450px;
+        margin: 25px auto;
+    }
 `;
 
 const Ranking = styled.div`
@@ -249,6 +335,19 @@ const PrizeText = styled.div<{ color: 'primary' | 'secondary'}>`
     text-transform: uppercase;
     @media only screen and (min-width: 768px) {
         font-size: 15px;
+    }
+`;
+
+const Formula = styled.div`
+    border: 2px solid #00b3d8;
+    max-width: 80%;
+    border-radius: 10px;
+    padding: 0 20px;
+    margin: 0 auto;
+    margin-bottom: 22px;
+    @media only screen and (min-width: 768px) {
+        max-width: 800px;
+        margin-bottom: 25px;
     }
 `;
 
@@ -293,6 +392,9 @@ const VideoRow = styled.div`
     @media only screen and (min-width: 750px) {
         margin-top: 25px;
         margin-bottom: 50px;
+        max-width: 800px;
+        margin-left: auto;
+        margin-right: auto;
     }
 `;
 
@@ -321,12 +423,6 @@ const CalendarIcon = styled.img`
     @media only screen and (min-width: 768px) {
         height: 16px;
     }
-`;
-
-const TickImage = styled.img`
-    position: relative;
-    top: 5px;
-    margin-right: 10px;
 `;
 
 const TwoColGridItem = styled.div`
