a1034c77b5795045908fd52e550722ab5be4efdc
naort-dev <naor.tzairi.upwork@gmail.com>
2020-11-17 09:34:26 +0000

    Fixing viewport meta tag to comfort accessibility requirements. (#57)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -89,7 +89,7 @@ class Trality extends App {
                             <GlobalStyle />
                             <Head>
                                 <meta charSet="UTF-8" />
-                                <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, user-scalable=no" />
+                                <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, maximum-scale=5.0" />
                                 <meta httpEquiv="content-language" content="en" />
                                 <meta httpEquiv="content-type" content="text/html; charset=utf-8" />
                                 <link rel="shortcut icon" href="/static/favicon.ico" />
