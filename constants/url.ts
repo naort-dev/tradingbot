940d8cd5920c643d830075fa8f70d5cfaae7fe92
Vadym <vadossm82@gmail.com>
2022-02-11 16:18:32 +0100

    update links to login and sign in pages

--- constants/url.ts
+++ constants/url.ts
@@ -1,4 +1,4 @@
 export enum ConstUrl {
-    Signup = 'https://auth.trality.com/#/signup',
-    Login = 'https://auth.trality.com/#/login',
+    Signup = 'https://account.trality.com/#/signup',
+    Login = 'https://account.trality.com/#/login',
 }
