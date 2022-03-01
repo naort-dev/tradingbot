07f3f02415a26e238acb967ae6a6dc8a7f99c8b6
naort-dev <naor.tzairi.upwork@gmail.com>
2022-03-01 09:52:13 +0100

    FIxing issue with banner.

--- containers/layout/competitionbanner.tsx
+++ containers/layout/competitionbanner.tsx
@@ -61,8 +61,6 @@ const CompCell = styled(Cell)`
     position: relative;
 `;
 
-const CompetitionUrl = typeof process.env.GATSBY_COMPETITION_URL === 'undefined' ? false : process.env.GATSBY_COMPETITION_URL;
-
 export const CompetitionBanner = () => {
     const [dismissed, setDismissed] = React.useState(() => (isDissmissed() ? true : false));
 
@@ -71,7 +69,7 @@ export const CompetitionBanner = () => {
         setSessionDismissed();
     };
 
-    if (dismissed || !CompetitionUrl) {
+    if (dismissed) {
         return null;
     }
     return (
